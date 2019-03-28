7592c5ae555f389dd08998d1d5c0705c749f186a
ZacLima <zacarias.lima@intitecperu.com>
2019-03-28 17:24:05 -0500

    reports move

--- models/it_reports_stock_move.py
+++ models/it_reports_stock_move.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
-
+import base64
+import datetime
+import logging
 from odoo import api, fields, models
 
 
@@ -16,3 +18,70 @@ class ItStockMoveReport(models.Model):
     @api.onchange("business_name")
     def _compute_it_ruc(self):
         self.vat = self.business_name.partner_id.vat or ""
+
+    @api.multi
+    def download_txt_units_sunat(self):
+        content = ""
+        count_sale = 0
+        #stock_move_before = self.env["stock.move.line"].search([("date", "<", self.date_in_time)])
+
+
+        stringventas = "%s|%s" % (
+            "Hola unidades fisicas",
+            "",  # campo 19
+        )
+        content += str(stringventas) + "\r\n"
+
+        nametxt = 'LE%s%s%s%s%s%s%s%s.TXT' % (
+            self.env.user.company_id.partner_id.vat,
+            '00',
+            '140100',
+            '00',
+            '1',
+            str(count_sale),
+            '1',
+            '1'
+        )
+        self.write({
+            "txt_filename": nametxt,
+            "txt_binary": base64.b64encode(bytes(content, "utf-8"))
+        })
+        return {
+            "name": "Report",
+            "type": "ir.actions.act_url",
+            "url": "web/content/?model=" + self._name + "&id=" + str(
+                self.id) + "&filename_field=file_name&field=txt_binary&download=true&filename=" + self.txt_filename,
+            "target": "new",
+        }
+
+    @api.multi
+    def download_txt_valuated_sunat(self):
+        content = ""
+        count_sale = 0
+        stringventas = "%s|%s" % (
+            "Hola inventario valorizado",
+            "",  # campo 19
+        )
+        content += str(stringventas) + "\r\n"
+
+        nametxt = 'LE%s%s%s%s%s%s%s%s.TXT' % (
+            self.env.user.company_id.partner_id.vat,
+            '00',
+            '140100',
+            '00',
+            '1',
+            str(count_sale),
+            '1',
+            '1'
+        )
+        self.write({
+            "txt_filename": nametxt,
+            "txt_binary": base64.b64encode(bytes(content, "utf-8"))
+        })
+        return {
+            "name": "Report",
+            "type": "ir.actions.act_url",
+            "url": "web/content/?model=" + self._name + "&id=" + str(
+                self.id) + "&filename_field=file_name&field=txt_binary&download=true&filename=" + self.txt_filename,
+            "target": "new",
+        }
