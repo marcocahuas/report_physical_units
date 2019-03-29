d2cb9b689dd481893b0f19142370fcf844986a39
ZacLima <zacarias.lima@intitecperu.com>
2019-03-29 10:44:22 -0500

    reports move

--- models/stock_move_line_inherit.py
+++ models/stock_move_line_inherit.py
@@ -6,3 +6,5 @@ from odoo import api, fields, models
 class StockMoveLineInherit(models.Model):
     _inherit = 'stock.move.line'
     _description = "Kardex de unidades fisicas"
+
+    stock_inicial = fields.Char()
