78b8cd8504ac72abfa2ec617507da4a271261dce
ZacLima <zacarias.lima@intitecperu.com>
2019-03-28 22:39:55 -0500

    reports move

--- models/it_stock_move_xlsx.py
+++ models/it_stock_move_xlsx.py
@@ -10,16 +10,31 @@ class ItStockMoveReport(models.AbstractModel):
     def generate_xlsx_report(self, workbook, data, move):
         for obj in move:
             # CREAR LA CABECERA
-            name = 'Unidades Fisicas - %s' % (obj.business_name)
-            sheet = workbook.add_worksheet(name)
-            font_titulo_empresa = workbook.add_format(
-                {
-                    'bold': True,
-                    'font_color': '#0B173B',
-                    'font_size': 14,
-                    'border': 4,
-                    'align': 'center',
-                    'valign': 'vcenter'
-                })
-            sheet.merge_range('A1:I4', self.env.user.company_id.name, font_titulo_empresa)
+            if obj.id is not False:
+                name = 'Unidades Fisicas - %s' % (obj.business_name)
+                sheet = workbook.add_worksheet(name)
+                font_titulo_empresa = workbook.add_format(
+                    {
+                        'bold': True,
+                        'font_color': '#0B173B',
+                        'font_size': 14,
+                        'border': 4,
+                        'align': 'center',
+                        'valign': 'vcenter'
+                    })
+                sheet.merge_range('A1:I4', self.env.user.company_id.name, font_titulo_empresa)
             # REPORTE STOCK MOVE
+            # COMPRAS
+            if obj.id is not False:
+                name = 'Inventario Valorizado - %s' % (obj.business_name)
+                sheet = workbook.add_worksheet(name)
+                font_titulo_empresa = workbook.add_format(
+                    {
+                        'bold': True,
+                        'font_color': '#0B173B',
+                        'font_size': 14,
+                        'border': 4,
+                        'align': 'center',
+                        'valign': 'vcenter'
+                    })
+                sheet.merge_range('A1:AC4', self.env.user.company_id.name, font_titulo_empresa)
