271e1267e5d05dffff63aaa2de468461114ff897
ZacLima <zacarias.lima@intitecperu.com>
2019-04-01 12:18:17 -0500

    reports move line

--- models/it_stock_move_xlsx.py
+++ models/it_stock_move_xlsx.py
@@ -8,17 +8,18 @@ class ItStockMoveReport(models.AbstractModel):
 
     def generate_xlsx_report(self, workbook, data, move):
         for obj in move:
-            # CREAR LA CABECERA
-            name = 'Inventario Valorizado - %s' % (obj.business_name)
-            sheet = workbook.add_worksheet(name)
-            font_titulo_empresa = workbook.add_format(
-                {
-                    'bold': True,
-                    'font_color': '#0B173B',
-                    'font_size': 14,
-                    'border': 4,
-                    'align': 'center',
-                    'valign': 'vcenter'
-                })
-            sheet.merge_range('A1:I4', self.env.user.company_id.name, font_titulo_empresa)
-            # REPORTE STOCK MOVE
+            if obj.id is not False:
+                # UNIDADES FISICAS
+                name = 'Inventario Valorizado - %s' % (obj.business_name)
+                sheet = workbook.add_worksheet(name)
+                font_titulo_empresa = workbook.add_format(
+                    {
+                        'bold': True,
+                        'font_color': '#0B173B',
+                        'font_size': 14,
+                        'border': 4,
+                        'align': 'center',
+                        'valign': 'vcenter'
+                    })
+                sheet.merge_range('A1:I4', self.env.user.company_id.name, font_titulo_empresa)
+                # REPORTE STOCK MOVE UNIDADES FISICAS
