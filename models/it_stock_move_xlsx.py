4b849c326a8ce415437b16630026386bcc70b9b5
ZacLima <zacarias.lima@intitecperu.com>
2019-04-01 12:33:41 -0500

    reports move line

--- models/it_stock_move_xlsx.py
+++ models/it_stock_move_xlsx.py
@@ -9,7 +9,7 @@ class ItStockMoveReport(models.AbstractModel):
     def generate_xlsx_report(self, workbook, data, move):
         for obj in move:
             if obj.id is not False:
-                # UNIDADES FISICAS
+                # CREAR LA CABECERA
                 name = 'Inventario Valorizado - %s' % (obj.business_name)
                 sheet = workbook.add_worksheet(name)
                 font_titulo_empresa = workbook.add_format(
@@ -23,3 +23,30 @@ class ItStockMoveReport(models.AbstractModel):
                     })
                 sheet.merge_range('A1:I4', self.env.user.company_id.name, font_titulo_empresa)
                 # REPORTE STOCK MOVE UNIDADES FISICAS
+                stock_move_before = self.env["it.units.move.report.phisical.line"].search(
+                    [("date", ">=", obj.date_in_time), ("date", "<=", obj.date_out_time)])
+
+                array_main = []
+                contador = 0
+                map_products_before = {}
+                for before_in in stock_move_before:
+                    if before_in.product_id.id is not False:
+                        str_product_id = str(before_in.product_id.id)
+                        map_products_before[str_product_id]
+
+                    array_field = []
+                    array_field.append(before_in.date)
+                    array_field.append(before_in.product_id.name)
+                    array_field.append("")
+                    array_field.append("")
+                    array_field.append(before_in.date)
+                    array_main.append(array_field)
+                    contador = contador + 1
+                sheet.set_column('A:I', 8)
+                row_name = 'A8:I%s' % (int(contador + 8))
+                sheet.add_table(row_name, {'data': array_main, 'columns': [{'header': 'FECHA'},
+                                                                           {'header': 'Producto'},
+                                                                           {'header': 'Entradas'},
+                                                                           {'header': 'Salidas'},
+                                                                           {'header': 'Saldo Final'},
+                                                                           ]})
