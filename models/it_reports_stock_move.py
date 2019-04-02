9c6216a82056ba4f1886d2cd5ae3e081bb6cc29e
ZacLima <zacarias.lima@intitecperu.com>
2019-04-02 16:26:40 -0500

    reports move line

--- models/it_reports_stock_move.py
+++ models/it_reports_stock_move.py
@@ -82,7 +82,7 @@ class ItStockMoveReport(models.Model):
             res_phisical = self.env["it.units.move.report.phisical.line"].sudo().create(json_stock_phisical)
         # ========================================================
         context = {'date': self.date_in_time}
-        entry_balance = self.env["account.move"].with_context(context).search(('move_id', '=', 'product'))
+        entry_balance = self.env["account.move"].with_context(context).search(('name', '=', 'name'))
         if entry_balance:
             for valor in entry_balance:
                 json_stock_phisical = {
