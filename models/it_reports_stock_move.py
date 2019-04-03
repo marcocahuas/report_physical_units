e7a474a0ddd08dcb993ca9059ad14c0f975a774e
ZacLima <zacarias.lima@intitecperu.com>
2019-04-03 13:56:47 -0500

    reports move line

--- models/it_reports_stock_move.py
+++ models/it_reports_stock_move.py
@@ -83,7 +83,7 @@ class ItStockMoveReport(models.Model):
         # ========================================================
 
         entry_balance = self.env["account.move.line"].search(
-            [("date", ">=", self.date_in_time), ("date", "<=", self.date_out_time), ('quantity', '!=', 'null')])
+            [("date", ">=", self.date_in_time), ("date", "<=", self.date_out_time), ('quantity', 'is', 'null')])
         if entry_balance:
             for valor in entry_balance:
                 json_stock_phisical = {
