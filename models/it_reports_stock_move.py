98230c994ccef9fdf3b3ca4961b981b555cd0020
ZacLima <zacarias.lima@intitecperu.com>
2019-04-04 11:12:46 -0500

    reports move line

--- models/it_reports_stock_move.py
+++ models/it_reports_stock_move.py
@@ -243,14 +243,14 @@ class ItStockMoveReport(models.Model):
                 "",
                 "",
                 stock_out.existence or 0,  # campo 6
-                stock_out.existence_id or "",  # campo 7
+                stock_out.existence_id,  # campo 7
                 "",  # campo 8
-                stock_out.date_gr or "",  # campo 10
-                stock_out.series or "",  # campo 11
-                stock_out.correlative or "",  # campo 12
-                stock_out.type_operation or "",  # campo 13 tipo operacion efect
-                stock_out.product_name or "",  # campo 14   descripcion de la exist
-                stock_out.units_med or "",  # campo 15  cod uni med
+                stock_out.date_gr,  # campo 10
+                stock_out.series,  # campo 11
+                stock_out.correlative,  # campo 12
+                stock_out.type_operation,  # campo 13 tipo operacion efect
+                stock_out.product_name,  # campo 14   descripcion de la exist
+                stock_out.units_med,  # campo 15  cod uni med
 
             )
             content += str(stringventas) + "\r\n"
