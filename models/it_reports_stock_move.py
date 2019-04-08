b74c0357780f0098b185cddf7e3221a758ff4271
ZacLima <zacarias.lima@intitecperu.com>
2019-04-08 16:02:38 -0500

    reports move line

--- models/it_reports_stock_move.py
+++ models/it_reports_stock_move.py
@@ -114,8 +114,10 @@ class ItStockMoveReport(models.Model):
 
                 if (location == 'internal') and (location_dest == 'internal'):
                     if before_in.picking_type_id.it_is_kardex is True:
-                        is_in_or_out = "in_entrada"
-                        type_operation_sunat = ""
+                        a = before_in.warehouse_id.it_establishment.code
+                        if (a == "0001") and (a == "0003"):
+                            is_in_or_out = "in_entrada"
+                            type_operation_sunat = ""
 
                 # INTERNAL DIF A UNA INTERNAL  TP = 00 =>SALIDA
                 if (location != 'internal') and (location_dest == 'internal'):
