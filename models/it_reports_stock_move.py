a19b011af79ed76f121dac5a9f7e0027a664182a
ZacLima <zacarias.lima@intitecperu.com>
2019-04-11 17:13:08 -0500

    reports move line

--- models/it_reports_stock_move.py
+++ models/it_reports_stock_move.py
@@ -179,7 +179,7 @@ class ItStockMoveReport(models.Model):
                     res_phisical = self.env["it.units.move.report.phisical.line"].sudo().create(json_stock_phisical)
 
                 if (a == 'internal') and (b == 'internal') \
-                        and (it_code is not False and before_in.location_id.is_kardex is False) \
+                        and (it_code is not False and before_in.location_id.is_kardex is not False) \
                         and (before_in.picking_type_id.it_is_kardex is True):
                     json_stock_phisical = {
                         "type": 0,
@@ -207,7 +207,7 @@ class ItStockMoveReport(models.Model):
                         json_stock_phisical)
 
                 if (a == 'internal') and (b == 'internal') \
-                        and (it_des_code is not False and before_in.location_dest_id.is_kardex is False) \
+                        and (it_des_code is not False and before_in.location_dest_id.is_kardex is not False) \
                         and (before_in.picking_type_id.it_is_kardex is True):
                     json_stock_phisical = {
                         "type": 0,
