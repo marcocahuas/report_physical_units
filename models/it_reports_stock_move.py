18b7685371b54313c39026415d1983b638e132ab
ZacLima <zacarias.lima@intitecperu.com>
2019-04-26 14:49:35 -0500

    reports move line

--- models/it_reports_stock_move.py
+++ models/it_reports_stock_move.py
@@ -460,13 +460,14 @@ class ItStockMoveReport(models.Model):
                 it_des_code = before_in.location_dest_id.it_establishment.code
                 type_operation_sunat = ""
                 type_operation_name = ""
+                metodo_coste = ""
                 is_scrap = before_in.location_dest_id.scrap_location
 
-                if before_in.product_id.categ_id.name == "average":
+                if before_in.product_id.categ_id.property_cost_method == "average":
                     metodo_coste = "1"
-                if before_in.product_id.categ_id.name == "fifo":
+                if before_in.product_id.categ_id.property_cost_method == "fifo":
                     metodo_coste = "2"
-                if before_in.product_id.categ_id.name == "standard":
+                if before_in.product_id.categ_id.property_cost_method == "standard":
                     metodo_coste = "3"
                 # PRODUCCION A UNA INTERNAL TP = 19 =>ENTRADA
                 if (a == "production") and (b == "internal"):
