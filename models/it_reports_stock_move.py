b76a2a45a5208a38812350966437990819ba4730
ZacLima <zacarias.lima@intitecperu.com>
2019-04-26 13:18:13 -0500

    reports move line

--- models/it_reports_stock_move.py
+++ models/it_reports_stock_move.py
@@ -108,8 +108,10 @@ class ItStockMoveReport(models.Model):
                     serie = stock_account_after.series.series
                     correlativo = stock_account_after.correlative
 
-                type_operation = self.env["type.of.operation"].search(['code', '=', '19'])
-                con_description = type_operation.description
+                type_operation = self.env["type.of.operation"].search(
+                    [("code", "=", before_in.picking_id.type_transaction.code or "-")], limit=1)
+                if type_operation.code == "01":
+                    venta_description = type_operation.description
 
                 # CAMPOS PARA IN OR OUT DE MOMIENTOS DE STOCK_MOVE
                 a = before_in.location_id.usage
@@ -123,7 +125,7 @@ class ItStockMoveReport(models.Model):
                 # PRODUCCION A UNA INTERNAL TP = 19 =>ENTRADA
                 if (a == "production") and (b == "internal"):
                     type_operation_sunat = "19"
-                    type_operation_name = con_description
+                    type_operation_name = "ENTRADA DE PRODUCCION"
                     fecha = before_in.date
                     tipo_doc = "00"
                     serie = "0"
@@ -139,11 +141,11 @@ class ItStockMoveReport(models.Model):
                 # INTERNAL A UN CLIENTE TP = 01 =>SALIDA
                 if (a == "internal") and (b == "customer"):
                     type_operation_sunat = "01"
-                    type_operation_name = "de"
+                    type_operation_name = venta_description
                 # CUSTOMER A INTERNAL ENTRADA X DEVOLUCION TP=24 => ENTRADA
                 if (a == "customer") and (b == "internal"):
                     type_operation_sunat = "24"
-                    type_operation_name = ""
+                    type_operation_name = "ENTRADA POR DEVOLUCIÓN DEL CLIENTE"
                 # INVENTORY A INTERNAL VS AJUSTES = 28 =>SALIDA
                 if (a == "inventory") and (b == "internal"):
                     type_operation_sunat = "28"
