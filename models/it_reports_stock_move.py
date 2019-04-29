c8d75b04e662cec637632c12911dc9eb76091278
ZacLima <zacarias.lima@intitecperu.com>
2019-04-29 15:22:18 -0500

    reports move line

--- models/it_reports_stock_move.py
+++ models/it_reports_stock_move.py
@@ -19,7 +19,7 @@ class ItStockMoveReport(models.Model):
     date_out_time = fields.Datetime(string='Fecha fin2')
     business_name = fields.Many2one('res.company', string='Razon Social')
     vat = fields.Char(string='RUC')
-    establishment = fields.Many2one('it.units.move.report.phisical.line', string='establecimiento')
+    establishment = fields.Many2one('it.units.move.report.phisical.line', string='establecimiento', limit=1)
     txt_filename = fields.Char()
     txt_binary = fields.Binary(string='Descargar Txt Sunat')
     # stock_move_lines = fields.Many2many(comodel_name="stock.move.line", string="Movimientos", ondelete="cascade")
