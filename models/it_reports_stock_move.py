236b031874182e079faaeeefe0a666507847e749
ZacLima <zacarias.lima@intitecperu.com>
2019-06-03 11:09:11 -0500

    reports move line

--- models/it_reports_stock_move.py
+++ models/it_reports_stock_move.py
@@ -75,41 +75,41 @@ class ItStockMoveReport(models.Model):
 
         for product in initial:
 
-            map_stabl = {}
-            for stock_quant in product.stock_quant_ids:
-                if stock_quant.location_id.it_establishment.id:
-                    if stock_quant.location_id.it_establishment.code not in map_stabl:
-                        map_stabl[stock_quant.location_id.it_establishment.code] = 0
-                    value_stock = map_stabl[stock_quant.location_id.it_establishment.code]
-                    value_stock = value_stock + stock_quant.quantity
-                    map_stabl[stock_quant.location_id.it_establishment.code] = value_stock
-
-            for code_estbl, quantity_total in map_stabl.items():
-                json_stock_phisical = {
-                    "type": 1,
-                    "date": self.date_in_time,
-                    "reference": "SALDO INICIAL",
-                    "is_saldo": "AAAA",
-                    "in_entrada": quantity_total,  # qyt_at_date
-                    "report_id": self.id,
-                    "product_id": product.id,
-                    # campos adicionales
-                    "stock_id": product.id,
-                    "establecimiento": code_estbl,
-                    "catalogo_existence": "9",
-                    "existence_id": "OTROS",
-                    "codigo_propio": "6000000000000000",
-                    "type_operation": code_transaction,
-                    "operation_name": description_transaction,
-                    "product_name": product.name,
-                    "date_gr": self.date_in_time,
-                    "catalog_01_id": "00",
-                    "series": "0",
-                    "correlative": "0",
-                    "existence": product.it_existence.code,
-                    "units_med": product.uom_id.code_unit_measure.code
-                }
-                res_phisical = self.env["it.units.move.report.phisical.line"].sudo().create(json_stock_phisical)
+            # map_stabl = {}
+            # for stock_quant in product.stock_quant_ids:
+            #     if stock_quant.location_id.it_establishment.id:
+            #         if stock_quant.location_id.it_establishment.code not in map_stabl:
+            #             map_stabl[stock_quant.location_id.it_establishment.code] = 0
+            #         value_stock = map_stabl[stock_quant.location_id.it_establishment.code]
+            #         value_stock = value_stock + stock_quant.quantity
+            #         map_stabl[stock_quant.location_id.it_establishment.code] = value_stock
+            #
+            # for code_estbl, quantity_total in map_stabl.items():
+            json_stock_phisical = {
+                "type": 1,
+                "date": self.date_in_time,
+                "reference": "SALDO INICIAL",
+                "is_saldo": "AAAA",
+                "in_entrada": product.qyt_at_date,
+                "report_id": self.id,
+                "product_id": product.id,
+                # campos adicionales
+                "stock_id": product.id,
+                "establecimiento": "0001",
+                "catalogo_existence": "9",
+                "existence_id": "OTROS",
+                "codigo_propio": "6000000000000000",
+                "type_operation": code_transaction,
+                "operation_name": description_transaction,
+                "product_name": product.name,
+                "date_gr": self.date_in_time,
+                "catalog_01_id": "00",
+                "series": "0",
+                "correlative": "0",
+                "existence": product.it_existence.code,
+                "units_med": product.uom_id.code_unit_measure.code
+            }
+            res_phisical = self.env["it.units.move.report.phisical.line"].sudo().create(json_stock_phisical)
         # ---------------------------------------------------
 
         # OBTENEMOS LOS MOVIMIENTOS
@@ -220,18 +220,6 @@ class ItStockMoveReport(models.Model):
                     if before_in.picking_id.correlative is False:
                         correlativo = before_in.picking_id.it_correlative_manual
 
-                # # Ajuste
-                # ajuste_fiscal = "0"
-                # date_comprobante = datetime.datetime.strptime(before_in.date, "%Y-%m-%d")
-                # mes_comprobante = str(date_comprobante.year) + "" + str("%02d" % (date_comprobante.month))
-                # mes_actual = str(d_ref.year) + "" + str(month)
-                # if before_in.picking_id.catalog_01_id.code == "01" or before_in.picking_id.catalog_01_id.code == "07":
-                #     ajuste_fiscal = "1"
-                #     if mes_actual != mes_comprobante:
-                #         ajuste_fiscal = "6"
-                # if before_in.picking_id.catalog_01_id.code == "03":
-                #     if mes_actual != mes_comprobante:
-                #         ajuste_fiscal = "7"
 
                 if (a == 'internal') and (b != 'internal'):
                     json_stock_phisical = {
